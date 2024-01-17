f8e67aedac41775998ba45f3f562ef90f02b1b47
Syllas Braga <syllasbraga2@gmail.com>
2024-01-17 11:44:14 -0300

    feat: get by document

--- src/main/java/com/sb/tech/controllers/TechnicianController.java
+++ src/main/java/com/sb/tech/controllers/TechnicianController.java
@@ -3,10 +3,7 @@ package com.sb.tech.controllers;
 import com.sb.tech.dtos.TechnicianDto;
 import com.sb.tech.services.TechnicianService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/technician")
@@ -21,5 +18,10 @@ public class TechnicianController {
     @GetMapping("/{id}")
     public ResponseEntity<TechnicianDto> getById(@PathVariable String id){
         return ResponseEntity.ok(TechnicianDto.toTechnicianDto(technicianService.getByUuid(id)));
+    }
+
+    @GetMapping("/document")
+    public ResponseEntity<TechnicianDto> getByDocument(@RequestParam String document){
+        return ResponseEntity.ok(TechnicianDto.toTechnicianDto(technicianService.getTechnicianByDocument(document)));
     }
 }
