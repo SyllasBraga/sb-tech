91126b5597062806a32641cc0700126bb732a1d4
Syllas Braga <syllasbraga2@gmail.com>
2024-01-18 14:53:40 -0300

    feat: Not found validation exception

--- src/main/java/com/sb/tech/controllers/advice/ControllerAdviceHandler.java
+++ src/main/java/com/sb/tech/controllers/advice/ControllerAdviceHandler.java
@@ -1,5 +1,6 @@
 package com.sb.tech.controllers.advice;
 
+import com.sb.tech.exceptions.NotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +13,13 @@ import java.time.Instant;
 
 @ControllerAdvice
 public class ControllerAdviceHandler {
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<StandardError> notFounException(NotFoundException e, HttpServletRequest http){
+        StandardError error = new StandardError(Instant.now(), HttpStatus.NOT_FOUND.value(),
+                "Not Found", e.getMessage(), http.getRequestURI());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ValidationError> validationException(MethodArgumentNotValidException e, HttpServletRequest http){
