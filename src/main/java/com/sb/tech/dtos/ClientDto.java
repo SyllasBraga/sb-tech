519b7aefd7d718525662e20bf83cd190946d3d40
Syllas Braga <syllasbraga2@gmail.com>
2024-01-23 15:38:17 -0300

    feat: Validations for ClientDto

--- src/main/java/com/sb/tech/dtos/ClientDto.java
+++ src/main/java/com/sb/tech/dtos/ClientDto.java
@@ -1,9 +1,13 @@
 package com.sb.tech.dtos;
 
 import com.sb.tech.models.ClientModel;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.br.CPF;
 
 import java.util.UUID;
 
@@ -12,9 +16,15 @@ import java.util.UUID;
 @Data
 public class ClientDto{
     private UUID id;
+    @CPF(message = "Invalid document.")
     private String document;
+    @NotBlank(message = "Field 'name' cannot be blank.")
+    @Size(min = 3, max = 255)
     private String name;
+    @NotBlank(message = "Field 'phone' cannot be blank.")
     private String phone;
+    @Email(message = "Invalid email.")
+    @NotBlank(message = "Field 'email' cannot be blank.")
     private String email;
 
     public static ClientDto toClientDto(ClientModel clientModel) {
