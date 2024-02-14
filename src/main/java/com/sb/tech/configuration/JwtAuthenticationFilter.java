c9470711c0a61a96e2e6cedfad33925eb38da303
Syllas Braga <syllasbraga2@gmail.com>
2024-02-14 10:43:39 -0300

    feat: JwtAuthenticationFilter

---
+++ src/main/java/com/sb/tech/configuration/JwtAuthenticationFilter.java
@@ -0,0 +1,59 @@
+package com.sb.tech.configuration;
+
+import com.sb.tech.services.JwtService;
+import com.sb.tech.services.TechnicianService;
+import io.micrometer.common.lang.NonNull;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+    private final TechnicianService technicianService;
+
+    public JwtAuthenticationFilter(JwtService jwtService, TechnicianService technicianService) {
+        this.jwtService = jwtService;
+        this.technicianService = technicianService;
+    }
+
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request,
+                                    @NonNull HttpServletResponse response, @NonNull FilterChain filterChain)
+            throws ServletException, IOException {
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        final String technicianDocument;
+        if (StringUtils.isEmpty(authHeader) || !StringUtils.startsWith(authHeader, "Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        jwt = authHeader.substring(7);
+        technicianDocument = jwtService.extractUserName(jwt);
+        if (StringUtils.isNotEmpty(technicianDocument)
+                && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = technicianService.getTechnicianByDocument(technicianDocument);
+            if (jwtService.isTokenValid(jwt, userDetails)) {
+                SecurityContext context = SecurityContextHolder.createEmptyContext();
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                        userDetails, null, userDetails.getAuthorities());
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                context.setAuthentication(authToken);
+                SecurityContextHolder.setContext(context);
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+}
