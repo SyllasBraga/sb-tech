f939d778f73d200fece6a97465d821e1c0518889
Syllas Braga <syllasbraga2@gmail.com>
2024-02-14 11:04:04 -0300

    feat: AuthenticationController

---
+++ src/main/java/com/sb/tech/controllers/AuthenticationController.java
@@ -0,0 +1,27 @@
+package com.sb.tech.controllers;
+
+import com.sb.tech.api.reponse.JwtAuthenticationResponse;
+import com.sb.tech.api.request.SigningRequest;
+import com.sb.tech.services.AuthenticationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+public class AuthenticationController {
+
+    private final AuthenticationService authenticationService;
+
+    public AuthenticationController(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
+
+    @PostMapping("/signin")
+    public ResponseEntity<JwtAuthenticationResponse> signing(@RequestBody SigningRequest request) {
+        return ResponseEntity.ok(authenticationService.signing(request));
+    }
+}
