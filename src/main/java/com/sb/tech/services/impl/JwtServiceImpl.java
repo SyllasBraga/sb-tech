002b7fd32c4932cd9250945b883d9e14cc416ad2
Syllas Braga <syllasbraga2@gmail.com>
2024-02-15 09:59:16 -0300

    feat: Jwt customized

--- src/main/java/com/sb/tech/services/impl/JwtServiceImpl.java
+++ src/main/java/com/sb/tech/services/impl/JwtServiceImpl.java
@@ -1,14 +1,22 @@
 package com.sb.tech.services.impl;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ser.std.JsonValueSerializer;
 import com.sb.tech.services.JwtService;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import io.swagger.v3.core.util.Json;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.gson.GsonBuilderCustomizer;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.http.converter.json.GsonFactoryBean;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 
 import java.security.Key;
 import java.util.Date;
@@ -21,6 +29,11 @@ public class JwtServiceImpl implements JwtService {
 
     @Value("${token.signing.key}")
     private String jwtSigningKey;
+    private final ObjectMapper objectMapper;
+
+    public JwtServiceImpl(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
 
     @Override
     public String extractUserName(String token) {
@@ -44,10 +57,15 @@ public class JwtServiceImpl implements JwtService {
     }
 
     private String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
-        return Jwts.builder().setClaims(extraClaims).setSubject(userDetails.getUsername())
-                .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
-                .signWith(getSigningKey(), SignatureAlgorithm.HS256).compact();
+        try {
+            return Jwts.builder()
+                    .setClaims(extraClaims).setSubject(objectMapper.writeValueAsString(userDetails))
+                    .setIssuedAt(new Date(System.currentTimeMillis()))
+                    .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                    .signWith(getSigningKey(), SignatureAlgorithm.HS256).compact();
+        } catch (JsonProcessingException e) {
+            throw new JsonRuntimeException(e.getMessage());
+        }
     }
 
     private boolean isTokenExpired(String token) {
